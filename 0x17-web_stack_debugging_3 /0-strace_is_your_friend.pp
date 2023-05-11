45690f2829582b538f8df6c519544651bc23fc53
mirachuks <104357108+mirachuks@users.noreply.github.com>
2023-05-11 23:17:53 +0100

    commit 0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3 /0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Changes `phpp` extensions to the correct ones: `php`
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
