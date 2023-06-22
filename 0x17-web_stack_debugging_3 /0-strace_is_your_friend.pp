73155d722c6213f372a389634231b80fc1eefaa2
mirachuks <104357108+mirachuks@users.noreply.github.com>
2023-06-22 15:59:07 -0700

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3 /0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3 /0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-# Changes `phpp` extensions to the correct ones: `php`
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# replace "phpp" with "php"
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
