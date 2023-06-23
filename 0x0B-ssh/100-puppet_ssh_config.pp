73e70a31ebe458c48028250cc95cd8ce97b1b461
mirachuks <104357108+mirachuks@users.noreply.github.com>
2023-06-23 10:53:57 -0700

    commit file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,14 @@
-# Setting up my client config file
-include stdlib
+# This Puppet configuration allows a local SSH client connect to a server
+# -- without typing a password.
 
 file_line { 'Turn off passwd auth':
-    ensure => present,
-    path   => '/etc/ssh/ssh_config',
-    line   => '    PasswordAuthentication no',
-    replace => true,
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
 
-file_line { 'Delare identity file':
-    ensure => present,
-    path   => '/etc/ssh/ssh_config',
-    line   => '     IdentityFile ~/.ssh/school',
-    replace => true,
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
